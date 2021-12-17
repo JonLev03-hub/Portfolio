9f38450e4dee76052d8be1ddd433c3ab59f1af76
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-16 22:49:48 -0500

    cleaned up

--- js/script.js
+++ js/script.js
@@ -7,12 +7,3 @@ document.getElementById('resume').addEventListener("click", function() {
 document.querySelector('.close').addEventListener("click", function() {
 	document.querySelector('.resume').style.display = "none";
 });
-
-//collect information for portfolio items
-
-//open the website
-//scrape the list of pinned projects
-
-//scrape the list of projects if project isnt already in project list
-//for each project in project list scrape the Title,first paragraph, and languages, last commit date
-//for each project in project list append items scraped
