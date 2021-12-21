2aec639ab6c01ba8b98d3fd254c5eb2d1b0d7b80
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-21 15:56:22 -0500

    removed unused code

--- js/portfolio.js
+++ js/portfolio.js
@@ -21,15 +21,13 @@ function createItems() {
     let projects = document.getElementById("projects")
     repos.index.forEach(repo => {
         // define all of the items for the project
-        let title = document.createElement("h1");
-        title.textContent = repo;
         let desc = document.createElement("p")
         desc.textContent = repos[repo].desc;
         let link = repos[repo].link;
         //assign all of the items to the project
         let project = document.createElement('div')
         project.classList += "project";
-        project.insertAdjacentHTML("beforeend", `<h1>${repo}</h1>`)
+        project.insertAdjacentHTML("beforeend", `<h1>${repo.replaceAll("_"," ").replaceAll("-"," ")}</h1>`)
         project.appendChild(desc)
         project.insertAdjacentHTML("beforeend", `<a href="${link}" target = "_blank" rel="noopener noreferrer"><image class="git" src = "resources/github.png"></image></a>`);
         projects.appendChild(project)
